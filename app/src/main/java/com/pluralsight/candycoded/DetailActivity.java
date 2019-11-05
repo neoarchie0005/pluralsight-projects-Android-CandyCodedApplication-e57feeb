8f3d8f4299d251df2ebade7f4829f01477697ea6
neoarchie0005 <neoarchie0005@gmail.com>
2019-11-05 11:35:47 +0000

    Updated on 065/11/2019

--- app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/DetailActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -46,16 +47,16 @@ public class DetailActivity extends AppCompatActivity {
                     CandyEntry.COLUMN_NAME_DESC));
 
 
-            TextView textView = (TextView) this.findViewById(R.id.text_view_name);
+            TextView textView = this.findViewById(R.id.text_view_name);
             textView.setText(candyName);
 
-            TextView textViewPrice = (TextView) this.findViewById(R.id.text_view_price);
+            TextView textViewPrice = this.findViewById(R.id.text_view_price);
             textViewPrice.setText(candyPrice);
 
-            TextView textViewDesc = (TextView) this.findViewById(R.id.text_view_desc);
+            TextView textViewDesc = this.findViewById(R.id.text_view_desc);
             textViewDesc.setText(candyDesc);
 
-            ImageView imageView = (ImageView) this.findViewById(
+            ImageView imageView = this.findViewById(
                     R.id.image_view_candy);
             Picasso.with(this).load(mCandyImageUrl).into(imageView);
         }
@@ -68,7 +69,17 @@ public class DetailActivity extends AppCompatActivity {
         return true;
     }
 
-    // ***
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        createShareIntent();
+        return super.onOptionsItemSelected(item);
+    }
+
     // TODO - Task 4 - Share the Current Candy with an Intent
-    // ***
+    private void createShareIntent(){
+        Intent shareIntent = new Intent(Intent.ACTION_SEND);
+        shareIntent.setType("text/plain");
+        shareIntent.putExtra(Intent.EXTRA_TEXT, SHARE_DESCRIPTION + mCandyImageUrl + HASHTAG_CANDYCODED);
+        startActivity(shareIntent);
+    }
 }
--- app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/InfoActivity.java
@@ -12,51 +12,36 @@ import com.squareup.picasso.Picasso;
 
 public class InfoActivity extends AppCompatActivity {
 
-    TextView mTextViewAddress;
-    TextView mTextViewPhone;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info);
 
         Uri uri = Uri.parse("android.resource://com.codeschool.candycoded/" + R.drawable.store_front);
-        ImageView candyStoreImageView = (ImageView) findViewById(R.id.image_view_candy_store);
+        ImageView candyStoreImageView = findViewById(R.id.image_view_candy_store);
         Picasso.with(this).
                 load(uri).
                 into(candyStoreImageView);
 
-        createMapIntent();
-        createPhoneIntent();
-
     }
 
     // TODO - Task 2 - Launch the Google Maps Activity
-    public void createMapIntent(){
-        mTextViewAddress = (TextView) findViewById(R.id.text_view_address);
-        mTextViewAddress.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri mapIntentUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
-                Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapIntentUri);
-                mapIntent.setPackage("com.google.android.apps.maps");
-                startActivity(mapIntent);
-            }
-        });
+    public void createMapIntent(View view) {
+        Uri mapIntentUri = Uri.parse("geo:0,0?q=618 E South St Orlando, FL 32801");
+        Intent mapIntent = new Intent(Intent.ACTION_VIEW, mapIntentUri);
+        mapIntent.setPackage("com.google.android.apps.maps");
+
+        if (mapIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapIntent);
+        }
     }
 
 
     // TODO - Task 3 - Launch the Phone Activity
-    public void createPhoneIntent(){
-        mTextViewPhone = (TextView) findViewById(R.id.text_view_phone);
-        mTextViewPhone.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent phoneIntent = new Intent(Intent.ACTION_CALL);
-                phoneIntent .setData(Uri.parse("tel:0123456789"));
-                startActivity(phoneIntent);
-            }
-        });
+    public void createPhoneIntent(View view) {
+        Intent phoneIntent = new Intent(Intent.ACTION_DIAL);
+        phoneIntent.setData(Uri.parse("tel:0123456789"));
+        startActivity(phoneIntent);
     }
 
 }
